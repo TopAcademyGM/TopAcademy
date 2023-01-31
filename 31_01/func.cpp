8cf1468245e80685c497202438f3109329ca3959
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-01-31 16:38:40 +0000

    start day 31_01

---
+++ 31_01/func.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+int input_age();
+std::string check_age(const int &age);
+void print_mess(const std::string &mess);
+
+int input_age()
+{
+    int age;
+
+    std::cout << "Hello! Input your age: ";
+    std::cin >> age;
+    return age;
+}
+
+std::string check_age(const int &age)
+{
+    std::string mess = "";
+    if (age > 20) {
+        mess = "Too old!";
+    } else if (age > 14) {
+        mess = "Hi teen!";
+    }else {
+        mess = "Too young!";
+    }
+    return mess;
+}
+
+void print_mess(const std::string &mess)
+{
+    std::cout << mess << "\n";
+}
+
+int main()
+{
+    int age = input_age();
+    std::string mess = check_age(age);
+    print_mess(mess);
+
+    char sign;
+    std::cin >> sign; // = '+'
+    std::cout << sign << "\n";
+    return 0;
+}
