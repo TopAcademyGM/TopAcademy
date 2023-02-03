1d981494e5b85e70088c9c7c901dce196642e830
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-02-03 15:31:27 +0000

    while for

---
+++ 03_01/while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // int n = 10;
+    // while (n > 0) {
+    //     std::cout << n << "\n";
+    //     n = n - 1;
+    // }
+
+    for (int n = 5; n > 0; n = n - 1){
+        std::cout << n << "\n";
+    }
+
+    return 0;
+}
