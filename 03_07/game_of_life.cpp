30cfeb2626c9499bcac574f21e7189949b0c593f
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-03-07 16:50:47 +0000

    add amimation

--- 03_07/game_of_life.cpp
+++ 03_07/game_of_life.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 
 const int G_x = 30;
 const int G_y = 10;
@@ -63,7 +64,10 @@ void next_generation(int matrix[][G_x]) {
         for(int j = 0; j < G_x; ++j) {
             int count_life = count_live_cells(tmp_matrix, i, j);
             
-            if (count_life == 3) {
+            if (matrix[i][j] == 0 && count_life == 3) {
+                matrix[i][j] = 1;
+            }else if (matrix[i][j] == 1 && (count_life == 3 ||
+                                            count_life == 2)) {
                 matrix[i][j] = 1;
             } else {
                 matrix[i][j] = 0; 
@@ -75,7 +79,23 @@ void next_generation(int matrix[][G_x]) {
 int main() {
     srand(time(NULL));
     int matrix[G_y][G_x];
+    // int matrix[G_y][G_x] {
+    //     {0,0,1,0,0},
+    //     {0,0,0,1,0},
+    //     {0,1,1,1,0},
+    //     {0,0,0,0,0},
+    //     {0,0,0,0,0}
+    // };
 
     rand_full(matrix);
-    print_matrix(matrix);
+
+    while (1)
+    {
+        // char next;
+        print_matrix(matrix);
+        next_generation(matrix);
+        // std::cin >> next;
+        sleep(1);
+        system("clear");
+    }
 }
