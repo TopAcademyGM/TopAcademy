d934641a677d4681a3fa9e2f0dc8ac059c2435f3
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-03-14 15:28:29 +0000

    read file

--- 03_10/fstream.cpp
+++ 03_10/fstream.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char const *argv[])
-{
-    int a = 5;
-    // i - input (чтение)
-    // f - file
-    // stream - поток
-    std::ifstream fin;
-    std::string file_name;
+
+void std_input_file(std::ifstream &fin) {
     std::string str_test;
     std::string str_test2;
     int int_test = 55;
-
-    std::cout << "Input file name: ";
-    std::cin >> file_name;
-    std::cout << "File " << file_name << ":\n";
-    fin.open(file_name);
 
     fin >> str_test;
     fin >> int_test;
@@ -26,6 +15,48 @@ int main(int argc, char const *argv[])
               << int_test << " "
               << str_test2 << " "
               << fin.bad() << "\n";
+}
 
+// Выводит весь файл по названию в консоль
+void print_all_file(const std::string &file_name) {
+    // i - input (чтение)
+    // f - file
+    // stream - поток
+    // char temp;
+    std::string temp;
+    std::ifstream file_in;
+    file_in.open(file_name);
+
+    // std::getline()
+
+    while (file_in.good()) {
+        // std::cin >> temp;
+        // file_in >> temp;
+        std::getline(file_in, temp);
+        std::cout << temp << "\n";
+    }
+    file_in.close();
+}
+
+void std_line() {
+    std::string str;
+    std::getline(std::cin, str); // Читает до переноса строки (\n)
+    // std::cin >> str;  // Читает до любого пустого символа (пробел, таб, перенос строки)
+    std::cout << str;
+}
+
+int main(int argc, char const *argv[])
+{
+    // i - input (чтение)
+    // f - file
+    // stream - поток
+    std::string file_name;
+
+    std::cout << "Input file name: ";
+    std::cin >> file_name;
+    std::cout << "File " << file_name << ":\n";
+
+    print_all_file(file_name);
+    // std_line();
     return 0;
 }
