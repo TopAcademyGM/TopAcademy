d9658f378dac3369b9f48f7722a1515c6c47e2c0
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-03-31 16:22:56 +0000

    Rect

---
+++ 03_31/Rect.cpp
@@ -0,0 +1,17 @@
+
+class Rect;
+
+int main(int argc, char const *argv[])
+{
+    Rect empty;
+    Rect a(10, 15);
+    Rect b(a);
+
+    a = Rect(5,5);
+    cout << "A:\t" << a << "\n";
+    cout << "A + B:\t" << a + b << "\n";
+    if (a >= b) {
+        std::cout << "A >= B";
+    }
+    return 0;
+}
