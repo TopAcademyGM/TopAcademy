5cd87dd293d85acc2d1cea49b03036e40a56ee5c
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-04-07 16:37:49 +0000

    fix

--- 04_07/vector.cpp
+++ 04_07/vector.cpp
@@ -22,7 +22,7 @@ class Array {
     Array(const Array &other) : cap(other.cap), size(other.size) {
         arr = new T[this->cap];
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < other.size; ++i) {
             arr[i] = other->arr[i];
         }
     }
@@ -30,7 +30,7 @@ class Array {
     Array(std::initializer_list<T> list) : cap(list.size()), size(list.size()) {
         this->arr = new T[this-cap];
         int j = 0;
-        for (std::initializer_list<T>::iterator i = list.begin();
+        for (typename std::initializer_list<T>::iterator i = list.begin();
             i != list.end(); ++i) {
             this->arr[j] = *i;
             ++j;
@@ -38,7 +38,10 @@ class Array {
     }
 
     void print() {
-        
+        for (int i = 0; i < this->size; ++i) {
+            std::cout << this->arr[i] << " "; 
+        }
+        std::cout << "\n"; 
     }
 
 };
@@ -55,17 +58,24 @@ void print_conteiner(cont arr) {
 }
 
 int main() {
-    int *arr = new int[5];
-    // arr[1] = 7;
-    std::vector<int> vec({1,2,3,4});
-    std::vector<int> vec();
-    vec.insert(vec.begin() + 2, 5);
-
-    std::list<int> my_list({9,8,7,6});
-
-    print_conteiner(vec);
-    print_conteiner(my_list);
-    std::sort(vec.begin(), vec.end());
-    print_conteiner(vec);
+    Array<int> test_arr(3,6);
+    test_arr.print();
     return 0;
 }
+
+
+// int main() {
+//     int *arr = new int[5];
+//     // arr[1] = 7;
+//     std::vector<int> vec({1,2,3,4});
+//     std::vector<int> vec();
+//     vec.insert(vec.begin() + 2, 5);
+
+//     std::list<int> my_list({9,8,7,6});
+
+//     print_conteiner(vec);
+//     print_conteiner(my_list);
+//     std::sort(vec.begin(), vec.end());
+//     print_conteiner(vec);
+//     return 0;
+// }
