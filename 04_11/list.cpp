8e2fc69e7b322c824714deb93c9eab3e37dcdeb8
TopAcademyGM <121567936+TopAcademyGM@users.noreply.github.com>
2023-04-11 17:33:23 +0000

    list

--- 04_11/list.cpp
+++ 04_11/list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 template <typename T>
 class my_list {
@@ -18,10 +19,90 @@ class my_list {
     node *head; // голова (первый элемент)
     node *tail; // хвост  (последний элемент)
     int size;
-    
+
+    void ch_head(node *n_node) {
+        node *tmp = this->head;
+        this->head = n_node;
+        if (tmp != nullptr) {
+            tmp->prev = this->head;
+        }
+        this->head->next = tmp;
+        this->head->prev = this->tail;
+
+        this->tail->next = this->head;
+    }
+    node *node_by_index(int i) {
+        node *find_node = this->head;
+        while (i != 0) {
+            find_node = find_node->next;
+            --i;
+        }
+        return find_node;
+    }
+
+ public:
+    void insert(int index, const T& val) {
+        node *tmp = node_by_index(index);
+        node *new_node = new node(val, tmp, tmp->prev);
+        if (tmp != nullptr) {
+            if (tmp->prev != nullptr) {
+                tmp->prev->next = new_node;
+            }
+            tmp->prev = new_node;
+        }
+        this->size++;
+    }
+    my_list() : size(0), head(nullptr) {
+        tail = new node();
+    }
+    my_list(const std::initializer_list<T> &list) {
+        tail = new node();
+        this->size = 0;
+        for (auto i = list.begin(); i != list.end(); ++i) {
+            if (i == list.begin()) {
+                ch_head(new node(*i));
+                this->tail->prev = this->head;
+                this->head->next = this->tail;
+                this->size++;
+            } else {
+                insert(this->size, *i);
+            }
+        }
+    }
+    void erase(int index) {
+        node *tmp = node_by_index(index);
+        tmp->prev->next = tmp->next;
+        tmp->next->prev = tmp->prev;
+        delete tmp;
+        this->size--;
+    }
+
+    void print() {
+        for (node *i = this->head; i != this->tail; i=i->next) {
+            std::cout << i->value << " ";
+        }
+        std::cout << "\n";
+    }
+
+    ~my_list() {
+        node *i = this->head;
+        while (i != this->tail) {
+            node *tmp = i;
+            i = tmp->next;
+            delete tmp;
+        }
+        delete this->tail;
+    }
+
 };
 
 int main() {
-    
+    my_list<std::string> str_list({"Kat", "Dog", "Wolf", "Snake"});
+    std::cout << "test\n";
+    str_list.print();
+    str_list.insert(2, "Enot");
+    str_list.print();
+    str_list.erase(4);
+    str_list.print();
     return 0;
 }
